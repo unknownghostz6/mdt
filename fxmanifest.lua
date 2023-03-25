c3a46f9ceb3c1ff562a3a6a8c7f7d0af3e486516
unknownghostz6 <88034715+unknownghostz6@users.noreply.github.com>
2023-03-25 02:38:25 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -21,7 +21,7 @@ client_script "cl_mdt.lua"
 
 --======================================= VERSION CHECK =====================================================--
 
-version '2.0'
+version '1.1'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/unknownghostz6/mdt'
